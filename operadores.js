c90897b0d8acebfacec4f555bc749407de22457a
nildemarco <nildemarco@gmail.com>
2019-10-18 20:15:32 -0300

    primer commit

---
+++ operadores.js
@@ -0,0 +1,138 @@
+let numero1 = 3;
+
+let numero2 = 6;
+
+let resultado = numero1 + numero2;
+
+console.log (resultado);
+
+resultado = numero1 - numero2;
+
+console.log (resultado);
+
+resultado = numero1 * numero2;
+
+console.log (resultado);
+
+resultado = numero1 / numero2;
+
+console.log (resultado);
+
+resultado = numero1 % numero2;
+
+console.log (resultado);
+
+let numero = 9;
+
+resultado = numero * 1;
+
+console.log (resultado);
+
+resultado = numero * 2;
+
+console.log (resultado);
+
+resultado = numero * 3;
+
+console.log (resultado);
+
+resultado = numero * 4;
+
+console.log (resultado);
+
+resultado = numero * 5;
+
+console.log (resultado);
+
+resultado = numero * 6;
+
+console.log (resultado);
+
+resultado = numero * 7;
+
+console.log (resultado);
+
+resultado = numero * 8;
+
+console.log (resultado);
+
+resultado = numero * 9;
+
+console.log (resultado);
+
+resultado = numero * 10;
+
+console.log (resultado);
+
+let longitud = 10;
+
+resultado = longitud + longitud + longitud + longitud;
+
+console.log (resultado);
+
+// otra manera de hacerlo 
+
+resultado = longitud * 4;
+
+console.log (resultado);
+
+longitud = 5;
+
+resultado = longitud * 4;
+
+console.log (resultado);
+
+let lado1 = 10;
+
+let lado2 = 20;
+
+let lado3 = 5;
+
+resultado = lado1 + lado2 + lado3;
+
+console.log (resultado);
+
+let altura = 10;
+
+let base = 4;
+
+resultado = altura + altura;
+
+console.log (resultado);
+
+resultado = base * altura;
+
+console.log (resultado);
+
+let cantidadDePersonas = 100;
+ 
+let factorDePersonas = cantidadDePersonas + 5;
+
+cantidadDePersonas = factorDePersonas;
+
+console.log ("La cantidad de personas:" + cantidadDePersonas);
+
+factorDePersonas = cantidadDePersonas - 3;
+
+cantidadDePersonas = factorDePersonas; 
+
+console.log ("La cantidad de personas:" + cantidadDePersonas);
+
+factorDePersonas = cantidadDePersonas * 2;
+
+cantidadDePersonas = factorDePersonas; 
+
+console.log ("La cantidad de personas:" + cantidadDePersonas);
+
+let ingreso = 1000;
+
+let ganancia = 600;
+
+let impuesto = 500;
+
+
+
+
+ 
+
+
