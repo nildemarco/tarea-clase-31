935f0a5095d1cf59250bc889d06fe58e587eab2b
nildemarco <nildemarco@gmail.com>
2019-10-21 18:47:08 -0300

    complete los ejercicios

--- operadores.js
+++ operadores.js
@@ -5,23 +5,23 @@ let numero2 = 6;
 
 let resultado = numero1 + numero2;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero1 - numero2;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero1 * numero2;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero1 / numero2;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero1 % numero2;
 
-console.log (resultado);
+console.log(resultado);
 
 // Ejercicio 28
 
@@ -29,61 +29,61 @@ let numero = 9;
 
 resultado = numero * 1;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 2;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 3;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 4;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 5;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 6;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 7;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 8;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 9;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = numero * 10;
 
-console.log (resultado);
+console.log(resultado);
 
 let longitud = 10;
 
 resultado = longitud + longitud + longitud + longitud;
 
-console.log (resultado);
+console.log(resultado);
 
 // otra manera de hacerlo 
 
 resultado = longitud * 4;
 
-console.log (resultado);
+console.log(resultado);
 
 longitud = 5;
 
 resultado = longitud * 4;
 
-console.log (resultado);
+console.log(resultado);
 
 let lado1 = 10;
 
@@ -93,7 +93,7 @@ let lado3 = 5;
 
 resultado = lado1 + lado2 + lado3;
 
-console.log (resultado);
+console.log(resultado);
 
 let altura = 10;
 
@@ -101,11 +101,11 @@ let base = 4;
 
 resultado = altura + altura;
 
-console.log (resultado);
+console.log(resultado);
 
 resultado = base * altura;
 
-console.log (resultado);
+console.log(resultado);
 
 // Ejercicio 33
 
@@ -113,16 +113,16 @@ let cantidadDePersonas = 100;
  
 cantidadDePersonas += 5;
 
-console.log ("La cantidad de personas:" + " " + cantidadDePersonas);
+console.log("La cantidad de personas:" + " " + cantidadDePersonas);
 
 cantidadDePersonas -= 3;
 
 
-console.log ("La cantidad de personas:" + " " + cantidadDePersonas);
+console.log("La cantidad de personas:" + " " + cantidadDePersonas);
 
 cantidadDePersonas *= 2;
 
-console.log ("La cantidad de personas:" + " " + cantidadDePersonas);
+console.log("La cantidad de personas:" + " " + cantidadDePersonas);
 
 //Ejercicio 34
 
@@ -132,15 +132,53 @@ let ganancia = 600;
 
 let impuesto = 500;
 
+//objetivos del mes
+
+let ingresoMes = 800;
+
+let impuestosMes = 400;
+
+let gananciaMes = 600;
+
+console.log("Objetivo de ingresos:", ingreso >= ingresoMes);
+
+console.log("Objetivo de impuestos:", impuesto < impuestosMes);
+
+console.log("Bono de ganancias:", ganancia == gananciaMes);
+
 // Ejercicio 35
 
 let numeroDeCliente = 143245;
 
 let numeroDeClienteIngresado = '143245';
 
-console.log ( "El número ingresado por el cliente es correcto:", numeroDeCliente == numeroDeClienteIngresado);
+console.log( "El número ingresado por el cliente es correcto:", numeroDeCliente == numeroDeClienteIngresado);
 
-console.log ( "El número ingresado por el cliente es válido:", numeroDeCliente === numeroDeClienteIngresado);
+console.log( "El número ingresado por el cliente es válido:", numeroDeCliente === numeroDeClienteIngresado);
+
+// Ejercicio 36 
+
+let numeroUno = 10;
+
+let numeroDos = 20;
+
+console.log("La variable numeroUno y numeroDos tienen el mismo valor:", numeroUno == numeroDos);
+
+console.log("La variable numeroUno y numeroDos son distintas:", numeroUno !== numeroDos);
+
+//Ejercicio 37
+
+let promedioParaAprobar = 6;
+
+let promedioDelAlumno = 4;
+
+let puntos = promedioParaAprobar - promedioDelAlumno;
+
+console.log("El alumno tiene un promedio mayor o igual que el requerido:", promedioDelAlumno >= promedioParaAprobar);
+
+console.log(`Al alumno le faltaron ${puntos} para aprobar`);
+
+
 
 
 
